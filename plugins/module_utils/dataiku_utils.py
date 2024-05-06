31e580facee4cc19f308ee7cd5bcc57d02ea0185
Jean-Bernard Jansen <jean-bernard.jansen@dataiku.com>
2024-05-06 19:02:32 +0200

    Fix connect_to null case for key,host,port

--- plugins/module_utils/dataiku_utils.py
+++ plugins/module_utils/dataiku_utils.py
@@ -86,25 +86,28 @@ def get_client_from_parsed_args(module):
     from dataikuapi.dssclient import DSSClient
 
     args = MakeNamespace(module.params)
-    api_key = (
-        args.api_key
-        if args.api_key is not None
-        else args.connect_to.get("api_key", os.environ.get("DATAIKU_ANSIBLE_DSS_API_KEY", None))
-    )
+    api_key = os.environ.get("DATAIKU_ANSIBLE_DSS_API_KEY", None)
+    if args.api_key:
+        api_key = args.api_key
+    elif args.connect_to and "api_key" in args.connect_to:
+        api_key = args.connect_to["api_key"]
+    
     if api_key is None:
         module.fail_json(
             msg="Missing an API Key, either from 'api_key' parameter, 'connect_to' parameter or DATAIKU_ANSIBLE_DSS_API_KEY env var"
         )
-    port = (
-        args.port
-        if args.port is not None
-        else args.connect_to.get("port", os.environ.get("DATAIKU_ANSIBLE_DSS_PORT", "80"))
-    )
-    host = (
-        args.host
-        if args.host is not None
-        else args.connect_to.get("host", os.environ.get("DATAIKU_ANSIBLE_DSS_HOST", "127.0.0.1"))
-    )
+
+    port = os.environ.get("DATAIKU_ANSIBLE_DSS_PORT", "80")
+    if args.port:
+        port = args.port
+    elif args.connect_to and "port" in args.connect_to:
+        port = args.connect_to["port"]
+
+    host = os.environ.get("DATAIKU_ANSIBLE_DSS_HOST", "127.0.0.1")
+    if args.host:
+        host = args.host
+    elif args.connect_to and "host" in args.connect_to:
+        host = args.connect_to["host"]
 
     client = DSSClient(f"http://{args.host}:{port}", api_key=api_key)
 
